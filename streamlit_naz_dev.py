e609f96e3bdcbde704f074085ddc9e52f2fe8dd9
nazcat <nazli.uremek@gmail.com>
2024-12-02 14:41:22 -0500

    Update streamlit_naz_dev.py

--- streamlit_naz_dev.py
+++ streamlit_naz_dev.py
@@ -10,7 +10,7 @@ import matplotlib.ticker as mtick
 # load files for dashboard #
 ############################
 totals = pd.read_csv('data/totals.csv')
-# df_imp_evnt_agg = pd.read_csv('/data/install_event_binned_users.csv')
+# df_imp_evnt_agg = pd.read_csv('data/install_event_binned_users.csv')
 date_diff_totals = pd.read_csv('data/install_event_binned_date.csv')
 partners = pd.read_csv('data/marketing_partners_users.csv')
 states = pd.read_csv('data/states_totals.csv')
