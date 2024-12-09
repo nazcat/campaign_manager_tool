8527b6e10ef8bfdc4618dc7761a243c630fcffd1
nazcat <nazli.uremek@gmail.com>
2024-12-09 03:18:04 -0500

    Update campaign_manager_models_only.py

--- campaign_manager_models_only.py
+++ campaign_manager_models_only.py
@@ -37,7 +37,7 @@ st.header("I love you!")
 
 credentials = st.secrets["gcp_service_account"]
 # client = storage.Client.from_service_account_info(credentials)
-credentials_json = json.dumps(credentials)
+credentials_json = dict(json.dumps(credentials))
 
 # Test connection to the bucket
 # bucket_name = 'campaign_manager_tool'
